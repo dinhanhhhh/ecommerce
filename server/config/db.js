58c55048d547a599fba866ff561a95350c20dffa
Admin <truongdinhanh27@gmail.com>
2025-05-05 11:15:23 +0700

    new

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
-    console.log("MongoDB connected!");
+    console.log("✅ Connected to MongoDB Atlas");
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
   }
