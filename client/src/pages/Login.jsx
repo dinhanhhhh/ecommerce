9255cfc261a71779aeb1ef485a44598498d0ef13
Admin <truongdinhanh27@gmail.com>
2025-05-06 09:26:12 +0700

    fix 3

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -96,6 +96,7 @@ export default function Login() {
                 required
                 className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
                 placeholder="••••••••"
+                autocomplete="current-password"
               />
             </div>
           </div>
